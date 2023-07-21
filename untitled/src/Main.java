b0549e6ced1c6adacc8038f0fcf651837de24fa3
marpe11 <marcuspgk@gmail.com>
2023-07-20 22:05:40 -0300

    adicionando opcao dos ususarios inputarem valores nas notas

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-            double nota1 = 5;
-            double nota2 = 6.5;
-            double nota3 = 2.5;
-            double nota4 = 9.0;
 
-        double media = (nota1+nota2+nota3+nota4)/4;
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println(media);
+
+            System.out.println("Digite a nota 1: ");
+            double nota1 = scanner.nextDouble();
+
+            System.out.println("Digite a nota 2: ");
+            double nota2 = scanner.nextDouble();
+
+            System.out.println("Digite a nota 3: ");
+            double nota3 = scanner.nextDouble();
+
+            System.out.println("Digite a nota 4: ");
+            double nota4 = scanner.nextDouble();
+            
+            double media = (nota1+nota2+nota3+nota4)/4;
+
+            System.out.println("A média das notas é: " + media);
+
+            scanner.close();
 
 
 
